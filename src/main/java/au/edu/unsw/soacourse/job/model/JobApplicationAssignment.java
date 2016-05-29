cb134ed1f33ac1a88070c35693866f4426fe80a5
double-pHelix <led_python@hotmail.com>
2016-05-29 20:25:21 +1000

    Updated JobApplicationAssignment to use element instead of id for member

--- src/main/java/au/edu/unsw/soacourse/job/model/JobApplicationAssignment.java
+++ src/main/java/au/edu/unsw/soacourse/job/model/JobApplicationAssignment.java
@@ -9,7 +9,9 @@ public class JobApplicationAssignment {
     private String jobApplicationId;
     private JobApplication jobApplicationLink;
     private String reviewer1;
+    private TeamMemberProfile reviewer1Link;
 	private String reviewer2;
+	private TeamMemberProfile reviewer2Link;
 	private String link;
 	private String rel;
 	private boolean sendVersion = false;
@@ -39,18 +41,24 @@ public class JobApplicationAssignment {
 		this.link = "http://localhost:8080/RestfulJobService/jobappreviewassign/" + this.id;
 	}
 	public String getJobApplicationId() {
+		if(sendVersion == true)
+			return null;
 		return jobApplicationId;
 	}
 	public void setJobApplicationId(String jobApplicationId) {
 		this.jobApplicationId = jobApplicationId;
 	}
 	public String getReviewer1() {
+		if(sendVersion == true)
+			return null;
 		return reviewer1;
 	}
 	public void setReviewer1(String reviewer1) {
 		this.reviewer1 = reviewer1;
 	}
 	public String getReviewer2() {
+		if(sendVersion == true)
+			return null;
 		return reviewer2;
 	}
 	public void setReviewer2(String reviewer2) {
@@ -87,6 +95,32 @@ public class JobApplicationAssignment {
 
 	public void setJobApplicationLink(JobApplication jobApplicationLink) {
 		this.jobApplicationLink = jobApplicationLink;
+	}
+
+	public TeamMemberProfile getReviewer1Link() {
+		if(this.reviewer1 == null)
+			return null;
+		
+		this.reviewer1Link = new TeamMemberProfile(this.reviewer1);
+		
+		return reviewer1Link;
+	}
+
+	public void setReviewer1Link(TeamMemberProfile reviewer1Link) {
+		this.reviewer1Link = reviewer1Link;
+	}
+
+	public TeamMemberProfile getReviewer2Link() {
+		if(this.reviewer2 == null)
+			return null;
+		
+		this.reviewer2Link = new TeamMemberProfile(this.reviewer2);
+		
+		return reviewer2Link;
+	}
+
+	public void setReviewer2Link(TeamMemberProfile reviewer2Link) {
+		this.reviewer2Link = reviewer2Link;
 	}
 
 	
