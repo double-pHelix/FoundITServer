5ec6c5824d305b6f739de9fe630674cc79befe7e
double-pHelix <led_python@hotmail.com>
2016-05-29 20:18:31 +1000

    Update HiringTeam resource to use elements instead of ids for members.

--- src/main/java/au/edu/unsw/soacourse/job/model/HiringTeamStore.java
+++ src/main/java/au/edu/unsw/soacourse/job/model/HiringTeamStore.java
@@ -13,10 +13,15 @@ public class HiringTeamStore {
     private String companyProfileId;
     private CompanyProfile companyProfileLnk;
     private String member1id;
+    private TeamMemberProfile member1Link;
     private String member2id;
+    private TeamMemberProfile member2Link;
     private String member3id;
+    private TeamMemberProfile member3Link;
     private String member4id;
+    private TeamMemberProfile member4Link;
     private String member5id;
+    private TeamMemberProfile member5Link;
     private String link;
     private String rel;
 	private boolean sendVersion = false;
@@ -65,30 +70,40 @@ public class HiringTeamStore {
 	}
 	
 	public String getMember1id() {
+		if(sendVersion == true)
+			return null;
 		return member1id;
 	}
 	public void setMember1id(String member1id) {
 		this.member1id = member1id;
 	}
 	public String getMember2id() {
+		if(sendVersion == true)
+			return null;
 		return member2id;
 	}
 	public void setMember2id(String member2id) {
 		this.member2id = member2id;
 	}
 	public String getMember3id() {
+		if(sendVersion == true)
+			return null;
 		return member3id;
 	}
 	public void setMember3id(String member3id) {
 		this.member3id = member3id;
 	}
 	public String getMember4id() {
+		if(sendVersion == true)
+			return null;
 		return member4id;
 	}
 	public void setMember4id(String member4id) {
 		this.member4id = member4id;
 	}
 	public String getMember5id() {
+		if(sendVersion == true)
+			return null;
 		return member5id;
 	}
 	public void setMember5id(String member5id) {
@@ -126,6 +141,61 @@ public class HiringTeamStore {
 		this.companyProfileLnk = companyProfileLnk;
 		this.companyProfileId = companyProfileLnk.getId();
 		
+	}
+	@XmlElement(name = "teamMemberProfile")
+	public TeamMemberProfile getMember1Link() {
+		if(this.member1id == null)
+			return null;
+		
+		this.member1Link = new TeamMemberProfile(this.member1id);
+		return member1Link;
+	}
+	public void setMember1Link(TeamMemberProfile member1Link) {
+		this.member1Link = member1Link;
+	}
+	@XmlElement(name = "teamMemberProfile")
+	public TeamMemberProfile getMember2Link() {
+		if(this.member2id == null)
+			return null;
+		
+		this.member2Link = new TeamMemberProfile(this.member2id);
+		return member2Link;
+	}
+	public void setMember2Link(TeamMemberProfile member2Link) {
+		this.member2Link = member2Link;
+	}
+	@XmlElement(name = "teamMemberProfile")
+	public TeamMemberProfile getMember3Link() {
+		if(this.member3id == null)
+			return null;
+		
+		this.member3Link = new TeamMemberProfile(this.member3id);
+		return member3Link;
+	}
+	public void setMember3Link(TeamMemberProfile member3Link) {
+		this.member3Link = member3Link;
+	}
+	@XmlElement(name = "teamMemberProfile")
+	public TeamMemberProfile getMember4Link() {
+		if(this.member4id == null)
+			return null;
+		
+		this.member4Link = new TeamMemberProfile(this.member4id);
+		return member4Link;
+	}
+	public void setMember4Link(TeamMemberProfile member4Link) {
+		this.member4Link = member4Link;
+	}
+	@XmlElement(name = "teamMemberProfile")
+	public TeamMemberProfile getMember5Link() {
+		if(this.member5id == null)
+			return null;
+		
+		this.member5Link = new TeamMemberProfile(this.member5id);
+		return member5Link;
+	}
+	public void setMember5Link(TeamMemberProfile member5Link) {
+		this.member5Link = member5Link;
 	}
 
 
